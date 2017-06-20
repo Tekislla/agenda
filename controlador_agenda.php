5f64205745cf8ec53337afc9240e9ce6038ceb72
Tekislla <jplazarim@gmail.com>
2017-06-20 11:51:47 -0300

    Add files via upload

--- controlador_agenda.php
+++ controlador_agenda.php
@@ -1,1 +1,22 @@
+<?php
 //controlador agenda
+
+$contatos = file_get_contents("contatos.json");
+$contatos = json_decode($contatos, true);
+
+$contato = [
+    "id"       => uniqid(),
+    "nome"     => "joao",
+    "email"    => "jplazarim@gmail.com",
+    "telefone" => "(47)99198-5098"
+];
+
+
+array_push($contatos, $contato);
+
+
+$dados_json = json_encode($contatos, JSON_PRETTY_PRINT);
+
+file_put_contents("contatos.json", $dados_json);
+
+print_r($dados_json);
